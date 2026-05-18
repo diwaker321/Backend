b75ec598af04c2deb591c74566680d2ed717434d
diwaker321 <diwakeracadmic@gmail.com>
2026-05-18 15:38:36 +0530

    code has been pushed

--- src/app.js
+++ src/app.js
@@ -1,25 +1,61 @@
-const express = require("express")
+// const express = require("express")
 
+// const app = express()
+
+
+// app.use("/login" , (req , res)=>{
+//     res.send("Hello this is Login section")
+// })
+
+// app.use("/browse" , (req , res)=>{
+//     res.send("Hello this is browse section")
+// })
+
+// app.get("/" , (req , res)=>{
+//     res.send("Hello users this is home section")
+// })
+
+// app.use((req , res)=>{
+//     res.status(404).send("Page Not Found")
+// })
+
+
+// app.listen(3000 , ()=>{
+//     console.log("Server is listening in port : 3000");
+// })
+
+const express = require("express")
 const app = express()
 
 
-app.use("/login" , (req , res)=>{
-    res.send("Hello this is Login section")
+app.get("/",(req , res)=>{
+    res.send("This is home page")
 })
 
-app.use("/browse" , (req , res)=>{
-    res.send("Hello this is browse section")
+app.get("/about" , (req, res)=>{
+    res.send("this is about section page")
+}) 
+
+app.use("/signin/:username/:password" , (req , res)=>{
+    console.log('req: ', req.params);
+    res.send("this is sign in page ")
 })
 
-app.get("/" , (req , res)=>{
-    res.send("Hello users this is home section")
+app.get("/signout" , (req, res)=>{
+    res.send("this is sign out page")
 })
 
-app.use((req , res)=>{
-    res.status(404).send("Page Not Found")
+// app.use("/signout" , (req, res)=>{
+//     res.send("this is the use  sign out page")
+// })
+app.post("/signout" , (req , res)=>{
+    res.send("this is the post api of signout")
+})
+app.use((req ,res)=>{
+    res.status(404).send("Please check your route")
 })
 
-
-app.listen(3000 , ()=>{
-    console.log("Server is listening in port : 3000");
+app.listen(7000 , ()=>{
+    console.log("server is listening to port :7000");
+    
 })
