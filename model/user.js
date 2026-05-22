edbda0c470932f9a3c6b8f6e3134ab0fb4c3be40
diwaker321 <diwakeracadmic@gmail.com>
2026-05-22 15:32:14 +0530

    successfully created the schema

---
+++ model/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+    firstname:{
+        type : String,
+    },
+    lastname:{
+        type : String,
+    },
+    email:{
+        type : String,
+    },
+    password:{
+        type : String,
+    },
+    age:{
+        type : Number,
+    },
+    gender:{
+        type : String,
+    },
+})
+
+module.exports = mongoose.model("user" , UserSchema)
